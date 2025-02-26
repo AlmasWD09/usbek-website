16b45f60401b90da10f9efd6ca993c6096ec59ce
almas hossain <almashossain7384@gmail.com>
2025-02-26 09:32:21 +0600

    popular section fake json data add ok

--- src/components/home/MostPopular.jsx
+++ src/components/home/MostPopular.jsx
@@ -1,56 +1,19 @@
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { RiShoppingCartLine } from "react-icons/ri"
 import { MdArrowForwardIos } from "react-icons/md";
 import { Select, Space } from 'antd';
 
 const MostPopular = () => {
     const [showAll, setShowAll] = useState(false);
+    const [products, setProducts] = useState([])
 
-    const products = [
-        {
-            id: 1,
-            image: "https://i.ibb.co.com/RHRPfHS/Whats-App-Image-2024-12-17-at-10-08-22-b2f3597f.jpg",
-            title: "The Ball",
-            price: "34.00 CND",
-            quentity: "02",
-        },
-        {
-            id: 2,
-            image: "https://i.ibb.co.com/WKKQSLW/Whats-App-Image-2024-12-17-at-10-08-23-5ecec3e4.jpg",
-            title: "Ab Wheel",
-            price: "50.00 CND",
-            quentity: "03",
-        },
-        {
-            id: 3,
-            image: "https://i.ibb.co.com/Wy2ZL7q/Whats-App-Image-2024-12-17-at-10-08-23-a1bc66c1.jpg",
-            title: "The Ball",
-            price: "64.00 CND",
-            quentity: "05",
-        },
-        {
-            id: 4,
-            image: "https://i.ibb.co.com/cQ0fc94/Whats-App-Image-2024-12-17-at-10-08-24-bfe4283f.jpg",
-            title: "Ab Wheel",
-            price: "34.00 CND",
-            quentity: "01",
-        },
-        {
-            id: 5,
-            image: "https://i.ibb.co.com/WKKQSLW/Whats-App-Image-2024-12-17-at-10-08-23-5ecec3e4.jpg",
-            title: "The Ball",
-            price: "34.00 CND",
-            quentity: "04",
-        },
-        {
-            id: 6,
-            image: "https://i.ibb.co.com/RHRPfHS/Whats-App-Image-2024-12-17-at-10-08-22-b2f3597f.jpg",
-            title: "The Ball",
-            price: "34.00 CND",
-            quentity: "06",
-        },
-    ];
+    useEffect(() => {
+        fetch("/popularData.json") 
+            .then((response) => response.json())
+            .then((data) => setProducts(data))
+            .catch((error) => console.error("Error fetching data:", error));
+    }, []);
 
     const displayedProducts = showAll ? products : products.slice(0, 4);
     const handleViewAllClick = () => {
@@ -75,8 +38,8 @@ const MostPopular = () => {
                                 onChange={handleChange}
                                 options={[
                                     { value: 'all', label: 'All' },
-                                    { value: 'lucy', label: 'Lucy' },
-                                    { value: 'Yiminghe', label: 'yiminghe' },
+                                    { value: 'free', label: 'Free' },
+                                    { value: 'paid', label: 'Paid' },
                                 ]}
                             />
                         </Space>
