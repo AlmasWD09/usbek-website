7dba152409a3b18e2b57c351e4e0638e46d5921b
almas hossain <almashossain7384@gmail.com>
2025-02-26 13:06:04 +0600

    scroll bar added

--- src/pages/viewDetails/ViewDetails.jsx
+++ src/pages/viewDetails/ViewDetails.jsx
@@ -146,34 +146,36 @@ const ViewDetails = () => {
 
                     {/* left site video/text */}
                     <div className="col-span-2 md:col-span-1">
-                        <h1 className="font-Poppins md:text-[20px] lg:text-[24px] font-medium text-[#000000] pb-4">You might interest in related videos</h1>
-                        {allData.map((item, index) => {
-                            return (
-                                <div key={index} className="">
-                                    <div className="mb-4 lg:mb-[8px]">
+                        <h1 className="font-Poppins text-[20px] font-medium text-[#000000] pb-4">You might interest in related videos</h1>
+                        <div className={`max-h-[800px] overflow-y-auto ${allData.length > 6 ? "scrollbar-thin scrollbar-thumb-red-100" : ""}`}>
+                            {allData.map((item, index) => {
+                                return (
+                                    <div key={index} className="">
+                                        <div className="mb-4 lg:mb-[8px]">
 
-                                        <div className="grid grid-cols-1 lg:grid-cols-2 gap-2">
-                                            <div className="bg-red-300 md:col-span-1 h-full rounded">
-                                                <div className=" relative z-10">
-                                                    <img src={item.image} alt="" className="w-full h-full object-cover" />
-                                                    <span className="absolute z-20 bottom-0 right-0 m-2 px-2 py-1 rounded-md bg-[#999999] text-[12px] font-Roboto text-[#333333]">17:08</span>
+                                            <div className="grid grid-cols-1 lg:grid-cols-2 gap-2">
+                                                <div className="md:col-span-1 h-full rounded">
+                                                    <div className=" relative z-10">
+                                                        <img src={item.image} alt="" className="w-full h-full object-cover" />
+                                                        <span className="absolute z-20 bottom-0 right-0 m-2 px-2 py-1 rounded-md bg-[#999999] text-[12px] font-Roboto text-[#333333]">17:08</span>
+                                                    </div>
                                                 </div>
-                                            </div>
 
-                                            <div className="lg:col-span-1">
-                                                <h1 className="font-Roboto text-[14px] text-[#333333] ">Get the Best Discounts at Michail vs Robert's ...</h1>
-                                                <p className="font-Roboto text-[14px] text-[#666666]">Karla Blair</p>
-                                                <div className="flex items-center gap-3">
-                                                    <p className="font-Roboto text-[12px] text-[#666666]"><span>10.5k</span> views</p>
-                                                    <span className="h-2 w-2 bg-[#999999] rounded-full"></span>
-                                                    <p className="font-Roboto text-[12px] text-[#666666]"><span>2</span> weeks ago</p>
+                                                <div className="lg:col-span-1">
+                                                    <h1 className="font-Roboto text-[14px] text-[#333333] ">Get the Best Discounts at Michail vs Robert's ...</h1>
+                                                    <p className="font-Roboto text-[14px] text-[#666666]">Karla Blair</p>
+                                                    <div className="flex items-center gap-3">
+                                                        <p className="font-Roboto text-[12px] text-[#666666]"><span>10.5k</span> views</p>
+                                                        <span className="h-2 w-2 bg-[#999999] rounded-full"></span>
+                                                        <p className="font-Roboto text-[12px] text-[#666666]"><span>2</span> weeks ago</p>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                            )
-                        })}
+                                )
+                            })}
+                        </div>
                     </div>
                 </div>
             </div>
